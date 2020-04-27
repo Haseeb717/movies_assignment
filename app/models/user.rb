681609e193a2af279c2aec6f95b56e1f3710604e
haseeb <haseeb@yallamotor.com>
2020-04-28 02:50:59 +0500

    User Email validity

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 	has_many :purchases
 	has_many :items, through: :purchases
 
+	validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
 	def libaray
 		self.items.where('expire_at <?',Time.current).order('expire_at ASC')
 	end
