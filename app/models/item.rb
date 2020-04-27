c7069299d281bb71afde0e83e2073804fd9e1ce9
haseeb <haseeb@yallamotor.com>
2020-04-28 03:02:03 +0500

    Validations in Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+
+	validate_presence_of :plot,:title
+	
 	scope :movies, -> { where('type =?','movie').order('created_at ASC') }
 	scope :seasons, -> { where('type =?','season').order('created_at ASC') }
 	scope :list, -> { order('created_at ASC') }
